1105401da8966798c3631ce0fdb3c023b2f17bdc
abyanji <a.byanjil99@gmail.com>
2022-12-26 03:33:52 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+
+int DataEntry(string message)
+{
+    System.Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void AverageOfColumns(int[,] array)
+{
+    double average = 0;
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            average += array[i, j];
+        }
+        average = average / array.GetLength(1);
+        System.Console.Write(average + " ");
+        average = 0;
+    }
+}
+
+int rows = DataEntry("Enter the value of rows: ");
+int columns = DataEntry("Enter the value of columns: ");
+int[,] matrix = CreateRandomArray(rows, columns, 0, 10);
+PrintArray(matrix);
+AverageOfColumns(matrix);
