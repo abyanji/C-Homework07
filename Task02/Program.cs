f988199e77698f950a4ab62b9fde9d9e3f83062b
abyanji <a.byanjil99@gmail.com>
2022-12-26 03:22:30 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.Clear();
+
+int DataEntry(string message)
+{
+    System.Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FindVariable(int[,] array, int variable)
+{
+    bool check = true;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == variable)
+            {
+                System.Console.WriteLine($"{variable} is on the {i+1} row and in the {j+1} column");
+                check = false;
+            }
+        }
+    }
+    if (check) System.Console.WriteLine($"{variable} is not in the array");
+}
+
+int rows = DataEntry("Enter the value of rows: ");
+int columns = DataEntry("Enter the value of columns: ");
+int[,] matrix = CreateRandomArray(rows, columns, 0, 10);
+int variable = DataEntry("Enter the value of variable: ");
+PrintArray(matrix);
+FindVariable(matrix, variable);
